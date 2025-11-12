e3df6e79059b655b433fc9d214b749a1cddfb9c5
elizavetamorhat-dev <elizavetamorhat@gmail.com>
2025-11-12 18:37:12 +0200

    Initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect,Locator } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.waitForLoadState("load");
+
+  // Click the get started link.
+    const getStartedButton = page.locator("a[class^=getStarted_]");
+    await getStartedButton.click();
+
+  // Expects page to have a heading with the name of Installation.
+    const InstallationHeader: Locator = page.locator('header h1');
+    await expect(InstallationHeader).toBeVisible();
+    await expect(InstallationHeader).toHaveText("Installation");
+});
